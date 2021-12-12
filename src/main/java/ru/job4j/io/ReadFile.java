3ef95e99edf431af05f49b1abff9d1d492051dfd
Bitok85 <kan041185@gmail.com>
2021-12-12 19:26:02 +0300

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -13,7 +13,7 @@ public class ReadFile {
             String[] lines = text.toString().split(System.lineSeparator());
             for (String line : lines) {
                 System.out.println((Integer.parseInt(line) % 2 == 0)
-                        ? "is even" : "isn't even");
+                        ? line + " is even" : line + " isn't even");
             }
         } catch (Exception e) {
             e.printStackTrace();
