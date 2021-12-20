177929944232b2cdb93de9a1a49713146614d222
Bitok85 <kan041185@gmail.com>
2021-12-20 15:16:18 +0300

    4.2. Поиск дубликатов [#315066 #241392]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,9 +3,23 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        for (Map.Entry<FileProperty, List<Path>> entry : DuplicatesVisitor.getAllFiles().entrySet()) {
+            List<Path> duplicates = entry.getValue();
+            if (duplicates.size() > 1) {
+                for (Path path : duplicates) {
+                    if (duplicates.get(0).equals(path)) {
+                        System.out.println(path.getFileName());
+                    }
+                    System.out.println(path.toAbsolutePath());
+                }
+            }
+        }
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -6,19 +6,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private HashSet<FileProperty> allFiles = new HashSet<>();
+    private static Map<FileProperty, List<Path>> allFiles = new HashMap<>();
+
+    public static Map<FileProperty, List<Path>> getAllFiles() {
+        return allFiles;
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty((long) attrs.size(), file.getFileName().toString());
-        if (!allFiles.add(fileProperty)) {
-            System.out.println(file.toAbsolutePath());
+        List<Path> duplicates = new ArrayList<>();
+        if (!allFiles.containsKey(fileProperty)) {
+            duplicates.add(file);
+            allFiles.put(fileProperty, duplicates);
+        } else {
+            allFiles.get(fileProperty).add(file);
         }
         return super.visitFile(file, attrs);
     }
