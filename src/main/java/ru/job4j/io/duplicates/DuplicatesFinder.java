468248dbe0ca843cda9a51f087dfab7cea4e0ea5
Bitok85 <kan041185@gmail.com>
2021-12-21 11:30:22 +0300

    4.2. Поиск дубликатов [#315066 #241392]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -4,22 +4,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
-        for (Map.Entry<FileProperty, List<Path>> entry : DuplicatesVisitor.getAllFiles().entrySet()) {
+        Map<Path, List<Path>> rslMap = DuplicatesFinder.listOfDuplicates(DuplicatesVisitor.getAllFiles());
+        for (Map.Entry<Path, List<Path>> entry : rslMap.entrySet()) {
+            System.out.println(entry.getKey().toString());
+            entry.getValue().forEach(System.out::println);
+        }
+    }
+
+    public static Map<Path, List<Path>> listOfDuplicates(Map<FileProperty, List<Path>> map) {
+        Map<Path, List<Path>> rslMap = new HashMap<>();
+        for (Map.Entry<FileProperty, List<Path>> entry : map.entrySet()) {
             List<Path> duplicates = entry.getValue();
             if (duplicates.size() > 1) {
                 for (Path path : duplicates) {
                     if (duplicates.get(0).equals(path)) {
-                        System.out.println(path.getFileName());
+                        List<Path> tmpList = new ArrayList<>();
+                        tmpList.add(path.toAbsolutePath());
+                        rslMap.put(path.getFileName(), tmpList);
+                    } else {
+                        rslMap.get(path.getFileName()).add(path.toAbsolutePath());
                     }
-                    System.out.println(path.toAbsolutePath());
                 }
             }
         }
+        return rslMap;
     }
 }
