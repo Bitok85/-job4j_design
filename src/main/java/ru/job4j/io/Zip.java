3bdca2945bf0f3d53d19f0e4e0057d54829d9c38
Bitok85 <kan041185@gmail.com>
2021-12-24 10:53:32 +0300

    5.2. Архивировать проект [#861 #242785]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,16 +1,18 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public static void packFiles(String[] args, List<File> sources, File target) {
-        if (!checkSourceDir(args)) {
-            throw new IllegalArgumentException("Source directory doesn't exist");
-        }
+    private final static int PARAMSQUANTITY = 3;
+
+    public static void packFiles(List<File> sources, File target) {
         if (!target.isDirectory()) {
             throw new IllegalArgumentException("Target directory doesn't exist");
         }
@@ -27,24 +29,27 @@ public class Zip {
         }
     }
 
-    private static File[] fileTreeTraversal(File folder) {
-        File[] folderEntries = folder.listFiles();
-        for (File entry : folderEntries) {
-            if (entry.isDirectory()) {
-                fileTreeTraversal(entry);
-                continue;
+    private static List<File> listOf(File folder) {
+        List<File> rslList = new ArrayList<>();
+        File[] fileArray = folder.listFiles();
+        rslList.addAll(Arrays.asList(fileArray));
+        for (File file : fileArray) {
+            if (file.isDirectory()) {
+                rslList.addAll(listOf(file));
             }
         }
-        return folderEntries;
-    }
-
-    private static boolean checkSourceDir(String[] args) {
-        ArgsName argsName = ArgsName.of(args);
-        File sourceDir = new File(argsName.get("d"));
-        return sourceDir.isDirectory() && sourceDir.length() > 0;
+        return rslList;
     }
 
     public static void main(String[] args) {
+        if (args.length != PARAMSQUANTITY) {
+            throw new IllegalArgumentException("Missing some of income parameters");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        File sourceDir = new File(argsName.get("d"));
+        if (!sourceDir.isDirectory() && sourceDir.length() == 0) {
+            throw new IllegalArgumentException("Invalid source directory");
+        }
         packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
