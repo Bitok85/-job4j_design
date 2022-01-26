c6b77c12d37717474a5f2fbe1948e8f423aaa032
Bitok85 <kan041185@gmail.com>
2022-01-26 15:35:06 +0300

    5.2. Архивировать проект [#861 #242785]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -13,8 +14,7 @@ public class Zip {
     private static String exclude;
     private static String targetDir;
 
-    private static Map<File, String> fileZipPath = new HashMap<>();
-    private static List<File> listOfExceptions = new ArrayList<>();
+    private static List<Path> pathList = new ArrayList<>();
 
 
     public static void main(String[] args) throws IOException {
@@ -24,54 +24,31 @@ public class Zip {
         targetDir = argsName.get("o");
         validation(args);
         searchExceptions();
-        fileMapGen(new File(sourceDir));
-        packZip();
+        packFiles(pathList, Paths.get(targetDir + ".zip"));
+    }
+
+    public static void packFiles(List<Path> pathList, Path target) throws IOException {
+        Path zipDir = Files.createFile(target);
+        Path pathSource = Paths.get(sourceDir);
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipDir))) {
+            pathList.stream()
+                    .filter(path -> !Files.isDirectory(path))
+                    .forEach(path -> {
+                        ZipEntry zipEntry = new ZipEntry(pathSource.relativize(path).toString());
+                        try {
+                            zipOutputStream.putNextEntry(zipEntry);
+                            Files.copy(path, zipOutputStream);
+                            zipOutputStream.closeEntry();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
     }
 
     private static void searchExceptions() throws IOException {
         Path start = Paths.get(sourceDir);
-        Search.search(start, p -> p.toFile().getName().endsWith(exclude))
-                .forEach(p -> listOfExceptions.add(p.toFile()));
-    }
-
-    public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void packZip() {
-        for (Map.Entry<File, String> entry : fileZipPath.entrySet()) {
-            packSingleFile(entry.getKey(), new File(entry.getValue()));
-        }
-    }
-
-    private static void fileMapGen(File file) {
-        if (file.isFile() && !listOfExceptions.contains(file)) {
-            fileZipPath.put(file, zipPath(file.getAbsolutePath()) + ".zip");
-        } else if (file.isDirectory() && !file.getName().equals(new File(targetDir).getName())) {
-            File zipEntryDir = new File(zipPath(file.getAbsolutePath()));
-            if (!zipEntryDir.mkdir()) {
-                System.out.println("Cannot create directory " + zipEntryDir.toString());
-            }
-            File[] subDir = file.listFiles();
-            if (subDir != null) {
-                Arrays.stream(subDir)
-                        .forEach(Zip::fileMapGen);
-            }
-        }
-    }
-
-    private static String zipPath(String file) {
-        String sourceTail = new File(sourceDir).getName();
-        return new File(file).getName().equals(sourceTail)
-                ? targetDir + File.separator + sourceTail
-                : targetDir + File.separator + file.substring(sourceDir.length() + 1, file.length());
+        pathList.addAll(Search.search(start, p -> !p.toFile().getName().endsWith(exclude)));
     }
 
     private static void validation(String[] params) {
@@ -80,9 +57,6 @@ public class Zip {
         }
         if (!new File(sourceDir).exists()) {
             throw new IllegalArgumentException("Source directory doesn't exist");
-        }
-        if (!new File(targetDir).exists()) {
-            throw new IllegalArgumentException("Target directory doesn't exist");
         }
     }
 }
