db5e13effb82402d659ea476b6bf12ac0f712904
Bitok85 <kan041185@gmail.com>
2022-01-29 17:35:30 +0300

    7. Scanner [#504791 #257048]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,79 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+
+public class CSVReader {
+
+    private static List<String> outList = new ArrayList<>();
+    private static final String PRINT = "stdout";
+
+    public static void handle(ArgsName argsName) throws Exception {
+        File file = new File(argsName.get("path"));
+        String filterNames = argsName.get("filter");
+        String delimiter = argsName.get("delimiter");
+        try (Scanner sc = new Scanner(file)) {
+            String[] firstRow = sc.nextLine().split(delimiter);
+            List<Integer> columns = CSVReader.colIndexes(firstRow, filterNames);
+            StringJoiner sj = new StringJoiner(";");
+            columns.forEach(p -> sj.add(firstRow[p]));
+            outList.add(sj.toString());
+            while (sc.hasNextLine()) {
+                StringJoiner tmpSj = new StringJoiner(";");
+                String[] rowArr = sc.nextLine().split(delimiter);
+                columns.forEach(p -> tmpSj.add(rowArr[p]));
+                outList.add(tmpSj.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void action(String out) throws IOException {
+        if (out.equals(PRINT)) {
+            outList.forEach(System.out::println);
+        } else {
+            Files.write(Paths.get(out), outList, StandardOpenOption.CREATE);
+        }
+    }
+
+    private static List<Integer> colIndexes(String[] firstRow, String filterNames) {
+        List<Integer> rsl = new ArrayList<>();
+        String[] filter = filterNames.split(",");
+        for (int i = 0; i < firstRow.length; i++) {
+            for (String str : filter) {
+                if (str.equals(firstRow[i])) {
+                    rsl.add(i);
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    private static void validation(ArgsName paramsName, String[] params) {
+        if (params.length != 4) {
+            throw new IllegalArgumentException("Some of input parameters are missing");
+        }
+        if (!Files.exists(Paths.get(paramsName.get("path")))) {
+            throw new IllegalArgumentException("File doesn't exist");
+        }
+        if (!paramsName.get("out").equals("stdout")
+                && !Files.exists(Paths.get(paramsName.get("out")).getParent())) {
+            throw new IllegalArgumentException("Incorrect output parameter. Input " + PRINT + " "
+                    + " for console input or correct file path");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ArgsName argsName = ArgsName.of(args);
+        validation(argsName, args);
+        handle(argsName);
+        action(argsName.get("out"));
+    }
+
+}
