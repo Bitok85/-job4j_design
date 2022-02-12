de0bd0656a689e28191035de840e2e2253469204
Bitok85 <kan041185@gmail.com>
2022-02-12 11:53:24 +0300

    1. Схема прав пользователи и роли. [#1093]

--- data/insert.sql
+++ data/insert.sql
@@ -1,17 +1,17 @@
-insert into attachs(name) values('readme.txt');
+insert into category(name) values('18+');
 
-insert into comments(name) values('unchecked');
+insert into statee(name) values('open');
 
-insert into item(name, comments_id, attachs_id) values ('Med', 1, 1);
+insert into rules(name) values('remotely');
 
-insert into users(name, item_id) values ('Kot Matroskin', 1);
+insert into rolee(name) values('Admin');
 
-insert into role_(name, users_id) values ('Admin', 1);
+insert into role_rules(rolee_id, rules_id) values (1, 1);
 
-insert into rules(name) values ('remotely');
+insert into users(name, rolee_id) values('Med', 1);
 
-insert into role_rules(role_id, rules_id) values (1, 1);
+insert into item(name, users_id, category_id, statee_id) values('Matroskin', 1, 1, 1);
 
-insert into category(name, item_id) values('18+', 1);
+insert into attachs(name, item_id) values ('readme.txt', 1);
 
-insert into state(name, item_id) values('open');
+insert into commentss(name, item_id) values('unchecked', 1);
--- data/script.sql
+++ data/script.sql
@@ -1,17 +1,52 @@
-
-create table person(
+create table category (
 	id serial primary key,
-	name varchar(255),
-	age smallint,
-	birthDate date,
-	vacant boolean
+	name text
 );
 
-insert into person(name, age, birthdate, vacant)
-values ('Kot Matroskin', '20', '2001-04-02 ', 'true');
+create table statee (
+	id serial primary key,
+	name text
+);
 
-select * from person;
+create table rules (
+	id serial primary key,
+	name text
+);
 
-update person set name = 'Sharik the Dog';
+create table rolee (
+	id serial primary key,
+	name text
+);
 
-delete from person;
+create table users (
+	id serial primary key,
+	name text,
+	rolee_id int references rolee(id)
+);
+
+create table item (
+	id serial primary key,
+	name text,
+	users_id int references users(id),
+	category_id int references category(id),
+	statee_id int references statee(id)
+);
+
+create table attachs (
+	id serial primary key,
+	name text,
+	item_id int references item(id)
+);
+
+create table commentss (
+	id serial primary key,
+	name text,
+	item_id int references item(id)
+);
+
+create table role_rules (
+	id serial primary key,
+	rolee_id int references rolee(id),
+	rules_id int references rules(id)
+);
+
