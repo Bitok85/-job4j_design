deb633f74e3bf60c15c965430670e8748d18c580
Bitok85 <kan041185@gmail.com>
2022-03-18 15:47:46 +0300

    Хранилище машин [#1733 #276771]

--- data/car_select.sql
+++ data/car_select.sql
@@ -4,13 +4,13 @@ from car as t1 inner join car_body as t2 on t1.car_body_id = t2.id
 			   inner join transmission as t4 on t1.transmission_id = t4.id
 
 select t1.id, body_type, color
-from car_body as t1 full join car as t2 on t2.car_body_id = t1.id
+from car_body as t1 left join car as t2 on t2.car_body_id = t1.id
 where t2.id is null
 
 select t1.id, engine_type, vol
-from engine as t1 full join car as t2 on t2.engine_id = t1.id
+from engine as t1 left join car as t2 on t2.engine_id = t1.id
 where t2.id is null
 
 select t1.id, transmission_type
-from transmission as t1 full join car as t2 on t2.transmission_id = t1.id
+from transmission as t1 left join car as t2 on t2.transmission_id = t1.id
 where t2.id is null
