f7399c54835840500f71eb50fc281b6993aea78d
Bitok85 <kan041185@gmail.com>
2022-03-22 11:39:42 +0300

    0.1. Statement [#379306 #277853]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -11,26 +11,33 @@ import java.util.StringJoiner;
 
 public class TableEditor implements AutoCloseable {
     private Connection connection;
-    private static Properties properties = new Properties();
+    private static Properties properties;
     private static final Logger LOG = LoggerFactory.getLogger(TableEditor.class.getName());
 
-    public TableEditor(Properties properties) throws  IOException, SQLException, ClassNotFoundException {
-        TableEditor.properties = properties;
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
+        this.properties = properties;
         initConnection();
     }
 
     public static void main(String[] args) throws Exception {
-        TableEditor tableEditor = new TableEditor(properties);
-        tableEditor.createTable("Alex");
-        tableEditor.addColumn("Alex", "name", "text");
-        tableEditor.renameColumn("Alex", "name", "age");
-        tableEditor.dropColumn("Alex", "age");
-        tableEditor.dropTable("Alex");
-        tableEditor.close();
+        try {
+            properties = new Properties();
+            properties.load(new FileReader("./data/PSQL_login.properties"));
+        } catch (Exception e) {
+            LOG.error("Exception", e);
+        }
+        try (TableEditor tableEditor = new TableEditor(properties)) {
+            tableEditor.createTable("Alex");
+            tableEditor.addColumn("Alex", "name", "text");
+            tableEditor.renameColumn("Alex", "name", "age");
+            tableEditor.dropColumn("Alex", "age");
+            tableEditor.dropTable("Alex");
+        } catch (Exception e) {
+            LOG.error("Exception", e);
+        }
     }
 
-    private void initConnection() throws IOException, SQLException, ClassNotFoundException {
-        properties.load(new FileReader("./data/PSQL_login.properties"));
+    private void initConnection() throws SQLException, ClassNotFoundException {
         Class.forName(properties.getProperty("driver"));
         connection = DriverManager.getConnection(
                 properties.getProperty("url"),
@@ -39,63 +46,50 @@ public class TableEditor implements AutoCloseable {
         );
     }
 
-    public void createTable(String tableName) throws Exception {
+    private void statementBlock(String sql) throws Exception {
         try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (Exception e) {
+            LOG.error("Exception", e);
+        }
+    }
+
+    public void createTable(String tableName) throws Exception {
             String sql = String.join(
                     " ",
                     "create table", tableName, "(id serial primary key);");
-            statement.execute(sql);
+            statementBlock(sql);
             System.out.println(getTableScheme(connection, tableName));
-        } catch (Exception e) {
-            LOG.error("Exception", e);
-        }
-
     }
 
     public void dropTable(String tableName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            String sql = "drop table Alex;";
-            statement.execute(sql);
-            LOG.info("Table " + tableName + " has been dropped");
-        } catch (Exception e) {
-            LOG.error("Exception", e);
-        }
+        String sql = "drop table Alex;";
+        statementBlock(sql);
+        LOG.info("Table " + tableName + " has been dropped");
     }
 
     public void addColumn(String tableName, String columnName, String type) throws Exception {
-        try (Statement statement = connection.createStatement()) {
             String sql = String.join(
                     " ",
                     "alter table", tableName, "add column", columnName, type);
-            statement.executeUpdate(sql);
+            statementBlock(sql);
             System.out.println(getTableScheme(connection, tableName));
-        } catch (Exception e) {
-            LOG.error("Exception", e);
-        }
     }
 
     public void dropColumn(String tableName, String columnName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
             String sql = String.join(
                     " ",
                     "alter table", tableName, "drop column", columnName);
-            statement.executeUpdate(sql);
+            statementBlock(sql);
             System.out.println(getTableScheme(connection, tableName));
-        } catch (Exception e) {
-            LOG.error("Exception", e);
-        }
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
             String sql = String.join(
                     " ",
                     "alter table", tableName, "rename column", columnName, "to", newColumnName);
-            statement.executeUpdate(sql);
+            statementBlock(sql);
             System.out.println(getTableScheme(connection, tableName));
-        } catch (Exception e) {
-            LOG.error("Exception", e);
-        }
     }
 
     private static String getTableScheme(Connection connection, String tableName) throws Exception {
@@ -116,7 +110,6 @@ public class TableEditor implements AutoCloseable {
         }
         return buffer.toString();
     }
-
 
     @Override
     public void close() throws Exception {
