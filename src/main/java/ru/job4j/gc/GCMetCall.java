0e8d01b50eca04ad55e1fce03dc740e4846ff227
Bitok85 <kan041185@gmail.com>
2022-04-19 07:35:50 +0300

    1. Демонстрация работы GC [#1589]

--- src/main/java/ru/job4j/gc/GCMetCall.java
+++ src/main/java/ru/job4j/gc/GCMetCall.java
@@ -1,9 +1,15 @@
 package ru.job4j.gc;
 
 public class GCMetCall {
+
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
     public static void main(String[] args) {
         Person person = new Person();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
+            if (i % 100 == 0) {
+                System.out.println(ENVIRONMENT.freeMemory() / 1000);
+            }
             person = getPerson();
         }
     }
