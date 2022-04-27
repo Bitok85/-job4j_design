d52f0fa9b9e16abc4fef68f05107f4b8e013b8a5
Bitok85 <kan041185@gmail.com>
2022-04-27 18:05:00 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GenTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class GenTest {
+
+    @Ignore
+    @Test
+    public void whenGenerateThenString() {
+        Gen gen = new Gen();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("${name}", "Alex");
+        args.put("${subject}", "you");
+        assertThat(gen.produce(template, args), is("I am Alex, Who are you? "));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInstanceNotExistInMap() {
+        Gen gen = new Gen();
+        String template = "I am ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("${name}", "Alex");
+        gen.produce(template, args);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenValueIsNull() {
+        Gen gen = new Gen();
+        String template = "I am ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("${name}", "Alex");
+        args.put("${subject}", null);
+        gen.produce(template, args);
+    }
+}
